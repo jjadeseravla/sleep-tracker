f12c390ed684b6178ad341a83dabee4b9cfe1929
jade <jadealvares@hotmail.co.uk>
2021-04-12 10:37:09 +0100

    new sq component

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export const Square = () => {
+
+  return (
+    <div>
+      <div className="box"></div>
+    </div>
+  )
+}
+
+export default Square;
--- src/components/__tests__/grid.test.js
+++ src/components/__tests__/grid.test.js
@@ -1,1 +1,12 @@
 import 'grid' from '../grid.js';
+
+describe('Grid Tests', () => {
+
+  it('should give back array', () => {
+    const MONTH_LENGTH = 30;
+    const result = createMonthLength(MONTH_LENGTH);
+    const expected = [];
+    expect(result).toEqual(expected);
+  })
+
+})
